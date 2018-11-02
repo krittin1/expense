6ac9c51046ea5599a1b322cc151bf43e9df1be60
James    Campbell <krittin.kan1@ku.th>
2018-11-02 14:23:53 +0700

    Add files via upload

---
+++ springlab/src/main/java/JdbcSQLiteConnection.java
@@ -0,0 +1,46 @@
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcSQLiteConnection {
+
+    public static void main(String[] args) {
+        try {
+// setup
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:spendee.db";
+            Connection conn = DriverManager.getConnection(dbURL);
+            if (conn != null) {
+                System.out.println("Connected to the database....");
+// display database information
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+// execute SQL statements
+                System.out.println("----- Data in expense table -----");
+                String query = "Select * from expense";
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()) {
+                    String name = resultSet.getString(2);
+                    String surname = resultSet.getString(3);
+                    double totalIncome = resultSet.getDouble(4);
+
+                    System.out.println("name:"+name+" surname:"+surname+" : "+totalIncome);
+                }
+// close connection
+                conn.close();
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
